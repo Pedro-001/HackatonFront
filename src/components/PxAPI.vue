4dad9f1d80426ae63f7ee787e697fa7a39424996
Pedro <pedro.a.barriosmolina@gmail.com>
2020-11-28 01:35:42 -0600

    Multiple Upload  images al 100

--- src/components/PxAPI.vue
+++ src/components/PxAPI.vue
@@ -5,9 +5,9 @@
       </h1>
     
         <div>
-          <input  type="file"  @change="onFileSelected" />
-          <button @click="onUpload"> Upload</button>
-           </div>
+          <input  type="file" multiple  @change="onFileSelected" />
+          <button @click="onUpload(images)">Upload</button>
+        </div>
            
     <router-link
       class="mt-5 text-xl text-green-600 hover:underline content-center"
@@ -25,32 +25,44 @@ export default {
 
 data(){
     return{
-      selectedFile: null
+      selectedFile: null,
+      images: []
     }
   },
   methods:{
     onFileSelected(event){
       console.log("==================================");
       console.log(JSON.stringify(event.target));
-
-      this.selectedFile = event.target.files[0]
-            console.log("==================================");
-
+      this.selectedFile = event.target.files
+      for (let index = 0; index < this.selectedFile.length; index++) {
+        const element = this.selectedFile[index];
+        this.images.push(element);
+      }
+      console.log("==================================");
     },
-    onUpload(){
-      const formData = new FormData();
-      formData.append('image', this.selectedFile,this.selectedFile.name)
-      axios.post('https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/0cfacc40-5db4-468e-aa75-ce5c9e5b078c/detect/iterations/Hackathon%20modelo%201/image',
-      formData,{
-        headers: {
-          'Prediction-Key': 'd5255a630d514403811ceab4988dcc4f',
-          'Content-Type' : 'application/octet-stream'
-        }
-      })
-      .then(res=>{
-        console.log(res.data.predictions[0].probability)
-        console.log(res.data.predictions[0].tagName)
-      })
+    onUpload(images){
+      for (var i=0; i<images.length; i++){
+          const element = this.images[i];
+          const formData = new FormData();          
+          formData.append('image', element,element.name)
+          axios.post('https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/0cfacc40-5db4-468e-aa75-ce5c9e5b078c/detect/iterations/Hackathon%20modelo%201/image',
+          formData,{
+            headers: {
+              'Prediction-Key': 'd5255a630d514403811ceab4988dcc4f',
+              'Content-Type' : 'application/octet-stream'
+            }
+          })
+          .then(res=>{
+            if (res.data.predictions[0].probability > 0.95) {
+              console.log(res.data.predictions[0].probability)
+              console.log(res.data.predictions[0].tagName)
+              
+              }
+          })
+
+      }
+
+
     }
   }
 
