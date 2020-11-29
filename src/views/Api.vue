cbb7051e8d307e4de60d02d631052d8c1d46cd1e
Pedro <pedro.a.barriosmolina@gmail.com>
2020-11-29 02:23:14 -0600

    post: localidades y estaciones ok

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <PxAPI  />
+    <PxLocalidades/>
+    <PxEstaciones/>
 
+ 
   </div>
 </template>
 
 <script>
 import PxAPI from "@/components/PxAPI";
-
+import PxLocalidades from "@/components/PxLocalidades"
+import PxEstaciones from "@/components/PxEstaciones"
 export default {
   name: 'api',
-  components: { PxAPI },
+  components: { PxAPI, PxLocalidades,PxEstaciones },
   
  }
 </script>
