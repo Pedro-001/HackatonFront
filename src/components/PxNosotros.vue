9290ea97f9c5120c87c65a570f54de5a9b588fad
samuel blas <blas.rayon.samuel@gmail.com>
2020-11-29 04:33:21 -0600

    imagenes responsivas en about

---
+++ src/components/PxNosotros.vue
@@ -0,0 +1,35 @@
+<template>
+	
+     <div >
+    <img src="../assets/img/1.png"  alt="" class="responsive">
+    <img src="../assets/img/2.png" class="responsive" alt="">
+    <img src="../assets/img/3.png" class="responsive" alt="">
+    <img src="../assets/img/4.png" class="responsive" alt="">
+    <img src="../assets/img/5.png" class="responsive" alt="">
+      </div>
+</template>
+
+<script>
+export default {
+props: {
+    assets: {
+      type: Array,
+      default: () => [],
+    },
+
+  },
+ 
+  name: "PxNosotros"
+
+  
+};
+
+</script>
+
+
+<style scoped>
+.responsive{
+	width: 100%;
+}
+
+</style>
