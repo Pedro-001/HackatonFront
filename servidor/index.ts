78eda454ce1c5f3e73b014cad397dfefb4963da9
Roque Ayala Angel David <ingRoque98@gmail.com>
2020-11-29 01:04:07 -0600

    Configurando CORS en el servidor

--- servidor/index.ts
+++ servidor/index.ts
@@ -12,6 +12,22 @@ const port = 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 // Archivos estaticos
 app.use('/static', express.static(path.join(__dirname, 'files')));
 
