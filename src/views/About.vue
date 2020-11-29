ef232a73b73146bba3f2570a5c9710d30a7f4e3a
samuel blas <blas.rayon.samuel@gmail.com>
2020-11-29 00:39:58 -0600

    se creo seccion nosotros

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-col items-center">
-    <h1 class="text-gray-700 text-6xl">API detección de animales</h1>
+   <!-- <h1 class="text-gray-700 text-6xl">API detección de animales</h1>
     <p class="text-gray-600 text-xl text-center">
       Proyecto para obtener la clasificación de imagenes de animales 
       <br />Este proyecto utiliza: vue js, 
@@ -9,6 +9,13 @@
     <router-link
       to="/"
       class="mt-5 text-xl text-green-600 hover:underline"
-    >Volver a la pagina de Inicio</router-link>
+    >Volver a la pagina de Inicio</router-link>-->
+  </div>
+  <div>
+    <img src="../assets/img/1.png" alt="">
+    <img src="../assets/img/2.png" alt="">
+    <img src="../assets/img/3.png" alt="">
+    <img src="../assets/img/4.png" alt="">
+    <img src="../assets/img/5.png" alt="">
   </div>
 </template>
