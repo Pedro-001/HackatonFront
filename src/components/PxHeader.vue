e2ec677061f748d41364062fa181c29efbf14a49
samuel blas <blas.rayon.samuel@gmail.com>
2020-11-28 18:24:28 -0600

    login

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -25,7 +25,7 @@
             <router-link class="nav-link" to="/" >Inicio <span class="sr-only">(current)</span></router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <router-link class="nav-link" to="/log" >Iniciar Sesión </router-link>
           </li>
           <li class="nav-item">
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
